b128eb1b924d7c05703e53e3e9436ba8d13e4da1
Abner Tarso <abnertarsolima@gmail.com>
2022-05-10 20:06:06 -0300

    Terceiro projeto com JS

---
+++ javascript/javascript.js
@@ -0,0 +1,23 @@
+function initScroll() {
+    const scrollSuave = document.querySelectorAll('[data-scroll]');
+    const windowHalf = window.innerHeight * .9;
+
+    if(scrollSuave.length) {
+        function startScroll() {
+            scrollSuave.forEach((itens) => {
+                const sectionTop = itens.getBoundingClientRect().top;
+                const conta = (sectionTop - windowHalf) < 0;
+
+                if(conta) { 
+                    itens.classList.add('active');
+                }
+            });
+        }
+
+        startScroll()
+
+        window.addEventListener('scroll', startScroll);
+    }
+}
+
+initScroll();
